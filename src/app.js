c6d3eb2c4a3d996ebcbbd0abe549efa69ffbac68
Edson0012 <Edsonewertonrs1@gmail.com>
2022-09-03 18:37:56 -0300

    concluido requisitos de post/participants e get/participants

--- src/app.js
+++ src/app.js
@@ -1,16 +1,76 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import dontev from "dotenv";
-dontev.config();
+import joi from "joi";
+import dayjs from "dayjs"
+dotenv.config();
+
 const app = express();
 app.use(cors())
 app.use(express.json())
 
-const mongoCLient = new MongoClient(process.env.MONGO_URI);
-
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-MongoClient.connect().then(() => {
-    db = mongoCLient.db('')
+mongoClient.connect().then(() => {
+	db = mongoClient.db("batepapoUOL");
+});
+
+const userSchema = joi.object({
+    name: joi.string().required()
 })
+
+
+app.get('/participants', async (req, res) => {
+
+    const listParticipants = await db.collection('participants').find({}).toArray()
+
+    res.send(listParticipants)
+});
+
+
+app.post('/participants', async (req, res) => {
+    
+    try {
+        const user = req.body
+        const validation = userSchema.validate(user);
+        
+        if (validation.error) {
+            return res.status(402).send('name deve ser strings não vazio')
+        };
+
+        const confirmUser = await db.collection('participants').findOne(user)
+
+        if(confirmUser){
+            return res.status(409).send('usuario existente.')
+        } else {
+
+            await db.collection('participants').insertOne({
+                name: user.name,
+                lastStatus: Date.now(),
+            })
+        };
+        
+        await db.collection('mensagem').insertOne({
+            from: user.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss'),
+        })
+
+        return res.status(201)
+
+    }catch(error){
+        return res.status(500).send({message: "erro no servidor"})
+    }
+
+});
+
+app.post('messages', async (req, res) => {
+    const {to, text, type} = req.body;
+})
+
+app.listen(5000, () => console.log("server listen 5000")
+);
